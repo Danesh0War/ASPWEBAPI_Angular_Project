5e5f58606346fd9e2c5c9355ec47773f61f9356f
Daniel Riazanov <danesh.reza.official@gmail.com>
2024-02-20 13:33:38 +0200

    Initial commit

---
+++ cities-server/Controllers/CitiesController.cs
@@ -0,0 +1,33 @@
+﻿using cities_server.Models;
+using cities_server.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cities_server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        private ICitiesService _citiesService;
+
+        public CitiesController(ICitiesService citiesService)
+        {
+            _citiesService = citiesService;
+        }
+
+        [HttpGet]
+        public IEnumerable<City> Get()
+        {
+            return _citiesService.GetCities();
+        }
+
+        [HttpPost]
+        public IActionResult Post(City city)
+        {
+            _citiesService.AddCity(city);
+            return StatusCode(201);
+        }
+
+
+    }
+}
